e4da32afa3e7ab3b767ab7384414cd7da71ca801
theodoremoreland <theodoremoreland@yahoo.com>
2024-01-04 11:59:25 -0600

    selection now highlights + updated gauge colors

--- application/static/js/app.js
+++ application/static/js/app.js
@@ -107,19 +107,19 @@ function buildCharts(sample) {
           {
             from: 0,
             to: 3,
-            color: "#87CEEB",
+            color: "blueviolet",
             thickness: 20,
           },
           {
             from: 3,
             to: 6,
-            color: "#00BFFF",
+            color: "midnightblue",
             thickness: 20,
           },
           {
             from: 6,
             to: 9,
-            color: "#1E90FF",
+            color: "blue",
             thickness: 20,
           },
         ],
@@ -193,11 +193,16 @@ function init() {
 
   // Use the list of sample names to populate the select options
   d3.json("/names").then((sampleNames) => {
-    sampleNames.forEach((sample) => {
+    sampleNames.forEach((sample, index) => {
       const li = document.createElement("li");
 
+      if (index === 0) {
+        li.setAttribute("class", "sample selected");
+      } else {
+        li.setAttribute("class", "sample");
+      }
+
       li.setAttribute("data-value", sample);
-      li.setAttribute("class", "sample");
       li.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 -960 960 960"><path d="M400-720q-33 0-56.5-23.5T320-800q0-33 23.5-56.5T400-880q33 0 56.5 23.5T480-800q0 33-23.5 56.5T400-720Zm260 480q42 0 71-29t29-71q0-42-29-71t-71-29q-42 0-71 29t-29 71q0 42 29 71t71 29ZM864-80 756-188q-22 14-46 21t-50 7q-75 0-127.5-52.5T480-340q0-75 52.5-127.5T660-520q75 0 127.5 52.5T840-340q0 26-7 50t-21 46l108 108-56 56Zm-424 0v-121q15 24 35.5 44t44.5 36v41h-80Zm-160 0v-520q-61-5-121-14.5T40-640l20-80q84 23 168.5 31.5T400-680q87 0 171.5-8.5T740-720l20 80q-59 16-119 25.5T520-600v41q-54 35-87 92.5T400-340v10q0 5 1 10h-41v240h-80Z"/></svg><p>${sample}</p>`;
 
       selector.append(() => li);
@@ -213,9 +218,12 @@ function init() {
 
 document.querySelector("#sample-list").addEventListener("click", (event) => {
   const target = event.target;
+  const selected = document.querySelector(".selected");
   const newSample = Number(target.getAttribute("data-value"));
 
   if (newSample) {
+    selected?.classList.remove("selected");
+    target.classList.add("selected");
     // Fetch new data each time a new sample is selected
     buildCharts(newSample);
     buildMetadata(newSample);
