63a695238320150d7b0d01b7dc236f52ee7547b4
theodoremoreland <theodoremoreland@yahoo.com>
2024-01-07 11:37:57 -0600

    implemented mobile sidebar

--- application/static/js/app.js
+++ application/static/js/app.js
@@ -1,3 +1,24 @@
+const sidebarToggle = document.querySelector("#sidebar-toggle");
+const mobileSelectionContainer = document.querySelector(
+  ".mobile.selection-container"
+);
+
+sidebarToggle.addEventListener("click", () => {
+  const mobileSidebarState = sidebarToggle.getAttribute("data-sidebar-state");
+
+  if (mobileSidebarState === "closed") {
+    sidebarToggle.setAttribute("data-sidebar-state", "open");
+    sidebarToggle.textContent = "<";
+    sidebarToggle.style.left = "8.5rem";
+    mobileSelectionContainer.style.left = "0px";
+  } else {
+    sidebarToggle.setAttribute("data-sidebar-state", "closed");
+    sidebarToggle.textContent = ">";
+    mobileSelectionContainer.style.left = "-8.5rem";
+    sidebarToggle.style.left = "0px";
+  }
+});
+
 function buildMetadata(sample) {
   d3.json("/metadata/" + sample).then((sample_data) => {
     const { ETHNICITY, GENDER, AGE, BBTYPE } = sample_data;
@@ -195,7 +216,8 @@ function buildCharts(sample) {
 
 function init() {
   // Grab a reference to the sample list element
-  var selector = d3.select("#sample-list");
+  var selector = d3.select(".desktop.sample-list");
+  var mobileSelector = d3.select(".mobile.sample-list");
 
   // Use the list of sample names to populate the select options
   d3.json("/names").then((sampleNames) => {
@@ -212,6 +234,7 @@ function init() {
       li.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 -960 960 960"><path d="M400-720q-33 0-56.5-23.5T320-800q0-33 23.5-56.5T400-880q33 0 56.5 23.5T480-800q0 33-23.5 56.5T400-720Zm260 480q42 0 71-29t29-71q0-42-29-71t-71-29q-42 0-71 29t-29 71q0 42 29 71t71 29ZM864-80 756-188q-22 14-46 21t-50 7q-75 0-127.5-52.5T480-340q0-75 52.5-127.5T660-520q75 0 127.5 52.5T840-340q0 26-7 50t-21 46l108 108-56 56Zm-424 0v-121q15 24 35.5 44t44.5 36v41h-80Zm-160 0v-520q-61-5-121-14.5T40-640l20-80q84 23 168.5 31.5T400-680q87 0 171.5-8.5T740-720l20 80q-59 16-119 25.5T520-600v41q-54 35-87 92.5T400-340v10q0 5 1 10h-41v240h-80Z"/></svg><p>${sample}</p>`;
 
       selector.append(() => li);
+      mobileSelector.append(() => li.cloneNode(true));
     });
 
     // Use the first sample from the list to build the initial plots
@@ -222,19 +245,41 @@ function init() {
   });
 }
 
-document.querySelector("#sample-list").addEventListener("click", (event) => {
+// Two selection-containers are used because it felt the most intuitive when designing to accommodate
+// ...an edge case wherein the user is making selections between adjusting screen sizes.
+function handleSelection(event) {
   const target = event.target;
-  const selected = document.querySelector(".selected");
   const newSample = Number(target.getAttribute("data-value"));
 
   if (newSample) {
-    selected?.classList.remove("selected");
-    target.classList.add("selected");
+    const targets = document.querySelectorAll(`[data-value="${newSample}"]`); // there are 2 elements with the same data-value, one for desktop and one for mobile
+    const selected = document.querySelectorAll(".selected"); // there are 2 selected elements, one for desktop and one for mobile
+
+    selected?.forEach((element) => {
+      element.classList.remove("selected");
+    });
+
+    targets.forEach((target) => {
+      target.classList.add("selected");
+    });
     // Fetch new data each time a new sample is selected
     buildCharts(newSample);
     buildMetadata(newSample);
   }
-});
+}
+
+// Add event listener for the sample list on desktop and mobile
+document
+  .querySelector(".desktop.sample-list")
+  .addEventListener("click", (event) => {
+    handleSelection(event);
+  });
+
+document
+  .querySelector(".mobile.sample-list")
+  .addEventListener("click", (event) => {
+    handleSelection(event);
+  });
 
 // Initialize the dashboard
 init();
